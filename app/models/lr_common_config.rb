28be9a96ad3d651c7eb40324369af7184d92e876
Ian Dickinson <i.j.dickinson@gmail.com>
2016-05-30 14:56:44 +0100

    Add a mechanism for introducing variabilty to the shared applications via the Rails config. E.g, we want to have a common page layout but the title element needs to be settable for each different application. The solution is to put variable elements into the Rails.application.config, and access those values via a helper class LrCommonConfig.

---
+++ app/models/lr_common_config.rb
@@ -0,0 +1,57 @@
+# Encapsulates common configuration options across the LR open
+# data applications, and provides defaults where appropriate
+class LrCommonConfig
+
+  class << self
+    include ActionView::Helpers::UrlHelper
+
+    def page_title
+      config_option( :lr_page_title, "Land Registry house price data" )
+    end
+
+    def application_css_class
+      config_option( :lr_application_css_class, "lr-app" )
+    end
+
+    def lr_logo_link_href
+      config_option( :lr_logo_href, "https://www.gov.uk/government/organisations/land-registry/" )
+    end
+
+    def lr_logo_link_title
+      config_option( :lr_logo_title, "Land Registry home page" )
+    end
+
+    def lr_logo_link_image
+      config_option( :lr_logo_image, "lr_logo_221_80.png" )
+    end
+
+    def home_path
+      "/"
+    end
+
+    def app_link( app_name, app_title, request )
+      css_classes = ["lr-header--header-proposition--a"]
+      if request.original_url =~ /#{app_name}/
+        css_classes << "lr-header--header-proposition--a__active"
+      end
+
+      path = relative_url_root.gsub( /[^\/]*\Z/, app_name )
+      link_to( app_title, path, {class: css_classes.join( " " )})
+    end
+
+    def relative_url_root
+      Rails.application.config.relative_url_root || ""
+    end
+
+
+    def config_option( key, default )
+      opt = Rails.application.config.x.send( key.to_sym )
+      is_actual_config_option?( opt ) || default
+    end
+
+    def is_actual_config_option?( opt )
+      (opt.respond_to?( :"empty?" ) && opt.empty?) ? false : opt
+    end
+  end
+
+end
