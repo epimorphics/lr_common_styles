2442736056f152595daa953e3906f63e1f30cebd
Ian Dickinson <i.j.dickinson@gmail.com>
2016-05-30 17:36:32 +0100

    Ensure that dependent gems are loaded

--- lib/lr_common_styles.rb
+++ lib/lr_common_styles.rb
@@ -1,4 +1,14 @@
 require "lr_common_styles/engine"
 
+require "jquery-rails"
+require "lodash-rails"
+require "bootstrap-sass"
+require "font-awesome-rails"
+require "modernizr-rails"
+require "modulejs-rails"
+require 'govuk_template'
+require 'govuk_frontend_toolkit'
+require 'govuk_elements_rails'
+
 module LrCommonStyles
 end
