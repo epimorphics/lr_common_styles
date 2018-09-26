337f9a233c5d69dc2bfa1930b73ea2cc121cb8df
Ian Dickinson <ian.dickinson@epimorphics.com>
2018-09-26 11:57:33 +0100

    Update to get simple integration test to run without deprecation warnings or errors

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,33 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+
+# Removed link to rails/test_help since it's causing an error initialising
+# ActiveRecord, which we don't need. So I've just transcluded the contents
+# of that file
+# require "rails/test_help"
+
+require "active_support/test_case"
+require "action_controller"
+require "action_controller/test_case"
+require "action_dispatch/testing/integration"
+require "rails/generators/test_case"
+
+require "active_support/testing/autorun"
+
+class ActionController::TestCase
+  def before_setup # :nodoc:
+    @routes = Rails.application.routes
+    super
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  def before_setup # :nodoc:
+    @routes = Rails.application.routes
+    super
+  end
+end
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
