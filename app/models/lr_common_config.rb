15b19ed86895cbddcc7ce23184640716f2f1959e
Ian Dickinson <i.j.dickinson@gmail.com>
2020-10-01 12:00:30 +0100

    Improve Welsh localization support
    This commit improves Welsh-language localization in the shared styles
    for PPD, std-reports and the landing site. In particular, we now
    localize the text in the header menu and the footer. `LrCommonConfig`
    has been simplified by moving some standard messages into the locale
    message catalogues.
    
    A standard language-switcher partial is also provided, for those sites
    that can make use of it.

--- app/models/lr_common_config.rb
+++ app/models/lr_common_config.rb
@@ -6,55 +6,18 @@ class LrCommonConfig
   class << self
     include ActionView::Helpers::UrlHelper
 
-    def page_title
-      config_option(:lr_page_title, 'HM Land Registry house price data')
-    end
-
-    def application_css_class
-      config_option(:lr_application_css_class, 'lr-app')
-    end
-
-    def lr_logo_link_href
-      config_option(:lr_logo_href, 'https://www.gov.uk/government/organisations/land-registry/')
-    end
-
-    def lr_logo_link_title
-      config_option(:lr_logo_title, 'HM Land Registry home page')
-    end
-
-    def lr_logo_link_image
-      config_option(:lr_logo_image, 'hm_lr_logo.png')
-    end
-
-    def lr_logo_link_svg_image
-      config_option(:lr_logo_image, 'hm_lr_logo.svg')
-    end
-
-    def home_path
-      '/'
-    end
-
-    def app_link(app_name, app_title, request)
+    def app_link(app_name, request)
       css_classes = ['lr-header--header-proposition--a']
       if request.original_url =~ /#{app_name}/
         css_classes << 'lr-header--header-proposition--a__active'
       end
 
       path = relative_url_root.sub(%r{[^/]*\Z}, app_name)
-      link_to(app_title, path, class: css_classes.join(' '))
+      link_to(I18n.t("common.app.#{app_name}"), path, class: css_classes.join(' '))
     end
 
     def relative_url_root
       Rails.application.config.relative_url_root || '/'
-    end
-
-    def config_option(key, default)
-      opt = Rails.application.config.x.send(key.to_sym)
-      actual_config_option?(opt) || default
-    end
-
-    def actual_config_option?(opt)
-      opt.respond_to?(:'empty?') && opt.empty? ? false : opt
     end
   end
 end
