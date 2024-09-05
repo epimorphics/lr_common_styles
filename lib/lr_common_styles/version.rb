7d1aedd72d06ad3d35d944b65cad7d6d08de5873
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-09-05 15:58:46 +0100

    build: updated version patch cadence

--- lib/lr_common_styles/version.rb
+++ lib/lr_common_styles/version.rb
@@ -3,7 +3,7 @@
 module LrCommonStyles
   MAJOR = 1
   MINOR = 9
-  PATCH = 7
+  PATCH = 8
   SUFFIX = nil # nil or 'rc' or 'beta' or 'alpha' for pre-release versions
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "-#{SUFFIX}"}"
 end
