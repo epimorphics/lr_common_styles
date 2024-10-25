fe67d24c9a8f46f6dc89116d71f430603ab9053f
Bogdan Marc <marc.bogdan96@yahoo.com>
2024-10-25 12:58:58 +0100

    Bumped gem version

--- lib/lr_common_styles/version.rb
+++ lib/lr_common_styles/version.rb
@@ -3,7 +3,7 @@
 module LrCommonStyles
   MAJOR = 2
   MINOR = 1
-  PATCH = 0
+  PATCH = 1
   SUFFIX = nil # nil or 'rc' or 'beta' or 'alpha' for pre-release versions
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "-#{SUFFIX}"}"
 end
