03e94de6068d12c78386da1e4dbd2bf86c412464
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-02-28 11:59:47 +0000

    chore: update version to 2.1.4
    - Changed PATCH version from 3 to 4
    - Updated string literal freezing syntax for VERSION

--- lib/lr_common_styles/version.rb
+++ lib/lr_common_styles/version.rb
@@ -1,9 +1,9 @@
-# frozen-string-literal: true
+# frozen_string_literal: true
 
 module LrCommonStyles
   MAJOR = 2
   MINOR = 1
-  PATCH = 3
+  PATCH = 4
   SUFFIX = nil # nil or 'rc' or 'beta' or 'alpha' for pre-release versions
-  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "-#{SUFFIX}"}"
+  VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && "-#{SUFFIX}"}".freeze
 end
