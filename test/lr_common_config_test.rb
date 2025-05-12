0de692fdee42473b1f9b4b49381f107152fd7bbb
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-05-12 12:55:51 +0100

    test: Allows multiple classes in app link
    - checks the `app_link` helper for acceptance of an array of classes.
    - checks the flexibility of link generation and enabling the inclusion of multiple CSS classes in the generated HTML tag.

--- test/lr_common_config_test.rb
+++ test/lr_common_config_test.rb
@@ -8,7 +8,7 @@ class LrCommonConfigTest < ActiveSupport::TestCase
         mock_request.expects(:original_url).returns('/mock')
         mock_request.expects(:params).returns({})
 
-        link = LrCommonConfig.app_link('dummy', mock_request)
+        link = LrCommonConfig.app_link('dummy', mock_request, 'lr-header--header-proposition--a')
 
         _(link).must_match(%r{<a class="lr-header--header-proposition--a" href="/dummy">.*})
       end
@@ -18,9 +18,18 @@ class LrCommonConfigTest < ActiveSupport::TestCase
         mock_request.expects(:original_url).returns('/dummy')
         mock_request.expects(:params).returns({})
 
-        link = LrCommonConfig.app_link('dummy', mock_request)
+        link = LrCommonConfig.app_link('dummy', mock_request, 'lr-header--header-proposition--a')
 
         _(link).must_match(%r{<a class="lr-header--header-proposition--a lr-header--header-proposition--a__active" href="/dummy">.*})
+      end
+
+      it 'should generate a non-active link with multiple classes' do
+        mock_request = mock('request')
+        mock_request.expects(:original_url).returns('/mock')
+        mock_request.expects(:params).returns({})
+
+        link = LrCommonConfig.app_link('dummy', mock_request, ['lr-header--header-proposition--a', 'another-class'])
+        _(link).must_match(%r{<a class="lr-header--header-proposition--a another-class" href="/dummy">.*})
       end
 
       it 'should preserve the language option' do
@@ -28,7 +37,7 @@ class LrCommonConfigTest < ActiveSupport::TestCase
         mock_request.expects(:original_url).returns('/mock')
         mock_request.expects(:params).at_least_once.returns({ lang: 'elvish' })
 
-        link = LrCommonConfig.app_link('dummy', mock_request)
+        link = LrCommonConfig.app_link('dummy', mock_request, 'lr-header--header-proposition--a')
 
         _(link).must_match(%r{<a class="lr-header--header-proposition--a" href="/dummy\?lang=elvish">.*})
       end
