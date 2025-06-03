bf23463f13339fa57d339e08466a371143b86c5b
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-06-03 09:31:09 -0400

    chore: update spelling
    - Change "initializer" to "initialiser" for consistency

--- config/initializers/lr_common_styles_config.rb
+++ config/initializers/lr_common_styles_config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Configuration initializer for LR Common Styles gem
+# Configuration initialiser for LR Common Styles gem
 Rails.application.config.tap do |config|
   # Set the Google Analytics ID, if available
   # This can be set in the environment variables or left as nil
