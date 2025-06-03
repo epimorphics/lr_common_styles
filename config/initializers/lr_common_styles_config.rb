1bc7a4ce64f8468d21a4387301a3b47872752a7d
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-06-03 09:13:00 -0400

    feat: add analytics config initialization
    - Introduces a configuration initializer for analytics
    - Allows setting Google Analytics ID from environment variables

---
+++ config/initializers/lr_common_styles_config.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Configuration initializer for LR Common Styles gem
+Rails.application.config.tap do |config|
+  # Set the Google Analytics ID, if available
+  # This can be set in the environment variables or left as nil
+  config.google_analytics_id = ENV.fetch('GOOGLE_ANALYTICS_ID', nil)
+end
