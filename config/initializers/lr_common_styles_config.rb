ced1908e3fecbaa75ddff1f2bb33a76ebe88cb05
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2025-06-24 13:09:13 +0100

    fix: update default Google Analytics ID
    - Changes default ID for backwards compatibility
    - Uses a predefined ID if environment variable is not set

--- config/initializers/lr_common_styles_config.rb
+++ config/initializers/lr_common_styles_config.rb
@@ -3,6 +3,7 @@
 # Configuration initialiser for LR Common Styles gem
 Rails.application.config.tap do |config|
   # Set the Google Analytics ID, if available
-  # This can be set in the environment variables or left as nil
-  config.google_analytics_id = ENV.fetch('GOOGLE_ANALYTICS_ID', nil)
+  # This can be set in the environment variables or left as old value
+  # for backwards compatibility.
+  config.google_analytics_id = ENV.fetch('GOOGLE_ANALYTICS_ID', 'UA-21165003-6')
 end
